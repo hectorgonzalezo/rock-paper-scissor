d75094f864d7cc53a72d8311f3415990f2dc0163
Hector Gonzalez <gorozco.hector@gmail.com>
2022-07-05 16:41:33 -0400

    Create game function
    it plays 5 games if given the right input and announces the winner

--- script.js
+++ script.js
@@ -12,37 +12,90 @@ function computerPlay() {
 }
 
 
-    //Plays a round of the game
-    //compares user input with play chosen randomly
-    function playRound(playerSelection, computerSelection) {
+//Plays a round of the game
+//compares user input with play chosen randomly
+//Returns a string with the result
+function playRound(playerSelection, computerSelection) {
 
-        //capitalize both selections, so that there is no conflict with different cases
-        playerSelection = capitalize(playerSelection)
-        computerSelection = capitalize(computerSelection)
+    //capitalize both selections, so that there is no conflict with different cases
+    playerSelection = capitalize(playerSelection)
+    computerSelection = capitalize(computerSelection)
 
-        //check if it's a tie. If not, run function that calculates result
-        if (playerSelection === computerSelection) {
-            return "It's a tie!"
+    //check if it's a tie. If not, run function that calculates result
+    if (playerSelection === computerSelection) {
+        return "It's a tie!"
+    }
+    else {
+        switch (playerSelection) {
+            case 'Rock':
+                return winOrLoose(playerSelection, computerSelection, 'Scissors', 'Paper')
+                break
+            case 'Paper':
+                return winOrLoose(playerSelection, computerSelection, 'Rock', 'Scissors')
+                break
+            case 'Scissors':
+                return winOrLoose(playerSelection, computerSelection, 'Paper', 'Rock')
+                break
+            default:
+                return "That's not a valid option!"
         }
-        else {
-            switch (playerSelection) {
-                case 'Rock':
-                    return winOrLoose(playerSelection, computerSelection, 'Scissor', 'Paper')
-                case 'Paper':
-                    return winOrLoose(playerSelection, computerSelection,'Rock', 'Scissor')
-                case 'Scissor':
-                    return winOrLoose(playerSelection, computerSelection, 'Paper', 'Rock')
+    }
+}
+
+//This function avoids too many nested switches.
+function winOrLoose(playerSelection, computerSelection, winChoice, looseChoice) {
+    switch (computerSelection) {
+        case winChoice:
+            return `You Win! ${playerSelection} beats ${computerSelection}`
+            break
+        case looseChoice:
+            return `You Loose! ${computerSelection} beats ${playerSelection}`
+            break
+    }
+
+}
+
+
+//plays 5 rounds and reports a winner at the end
+function game() {
+    let ties = 0;
+    let wins = 0;
+    let loses = 0;
+    for (let i =0; i < 5;) {
+        let playerSelection = prompt("Enter your choice: Rock, Paper or Scissors")
+        let result = playRound(playerSelection, computerPlay())
+        console.log(result)
+
+        //only continue loop if input by user is valid
+        if (result != "That's not a valid option!") {
+            i++
+
+            switch (result.slice(4,7)) {
+                case 'Win':
+                    wins += 1;
+                    break
+                case 'Loo':
+                    loses += 1;
+                    break
+                case ' a ':
+                    ties += 1;
+                    break
             }
         }
     }
 
-    //This function avoids too many nested switches.
-    function winOrLoose(playerSelection, computerSelection, winChoice, looseChoice) {
-        switch (computerSelection) {
-            case winChoice:
-                return `You Win! ${playerSelection} beats ${computerSelection}`
-            case looseChoice:
-                return `You Loose! ${computerSelection} beats ${playerSelection}`
-        }
+    console.log(determineWinner(ties, wins, loses))
+    
+}
 
+function determineWinner(ties, wins, loses) {
+    if (wins > loses) {
+        return "Congratulations!  You win the game."
     }
+    else if (loses > wins) {
+        return "I'm sorry! You loose the game."
+    }
+    else {
+        return "The whole game is a tie."
+    }
+}
